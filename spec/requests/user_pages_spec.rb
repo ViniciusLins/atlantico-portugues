38ba3d64e00714983fca3bc948a1e8cc31c3025a
Nilton Vasques <nilton.vasques@gmail.com>
2015-03-10 08:08:26 -0300

    Create a signup form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,6 +8,37 @@ describe "User pages" do
 
     it { should have_selector('h1',     text: 'Sign up') }
     it { should have_selector('title',  text: 'Sign up') }
+
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    describe "when visit page " do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "when submit an blank form" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "when submit valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 
   describe "profile page" do
