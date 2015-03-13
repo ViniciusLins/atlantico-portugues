3a0062c9084dba82e1d27cd1e6463e5b8e1ef636
Nilton Vasques <nilton.vasques@gmail.com>
2015-03-13 14:29:15 -0300

    Remove mass assignmente test, because rails 4 move this funcionality to controller

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -24,14 +24,6 @@ describe Micropost do
 
   it { should be_valid }
 
-  describe "accessible attributes" do
-    it "should not allow access to user_id" do
-      expect do
-        Micropost.new(user_id: user.id) 
-      end.should raise_error
-    end
-  end
-
   describe "when user_id is not present" do
     before { @micropost.user_id = nil }
 
