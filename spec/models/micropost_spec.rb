950a4989ba7b5baff7fa5226b6d2e2436febd3ee
Nilton Vasques <nilton.vasques@gmail.com>
2015-03-18 12:52:38 -0300

    Add user following and improves feeds page

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -17,9 +17,11 @@ describe Micropost do
 
   subject { @micropost }
 
+  # attributes
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
   it { should respond_to(:user) }
+
   its(:user) { should == user }
 
   it { should be_valid }
