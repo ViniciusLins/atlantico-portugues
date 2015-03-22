1e27b160ae916ebd3e6d4de133999d078af55ef2
Nilton Vasques <nilton.vasques@gmail.com>
2015-03-22 02:53:12 -0300

    Fix identation in sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,9 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user end 
+    self.current_user = user 
+  end 
+
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
