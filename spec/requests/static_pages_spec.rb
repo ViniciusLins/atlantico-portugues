eb2bb26d99e214d989172d295cf056e8d718cbba
Vlins <vlinsufba@gmail.com>
2015-03-25 16:28:02 -0300

    All static strings of application and of rails framework - like validation comments strings -  were translated in locale file, expect invalid email string.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,9 +5,10 @@ describe "Static pages" do
   subject{ page }
 
   before(:all) do
-    @home_page = FactoryGirl.create(:page, title: "Home") 
-    @help_page = FactoryGirl.create(:page, title: "Ajuda") 
-    @about_page = FactoryGirl.create(:page, title: "Sobre Nós") 
+    @home_page = FactoryGirl.create(:page, title: I18n.t('home')) 
+    @help_page = FactoryGirl.create(:page, title: I18n.t('help')) 
+    @about_page = FactoryGirl.create(:page, title: I18n.t('about'))
+#    @contact_page = FactoryGirl.create(:page, title: I18n.t('contact'))
   end
 
   shared_examples_for "all static_pages" do
@@ -18,43 +19,43 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should_not have_selector('title', text: "| Home") }
+    it { should_not have_selector('title', text: I18n.t('home')) }
     it { should have_content(@home_page.body) }
   end
 
   describe "Ajuda page" do
     before { visit help_path }
 
-    let(:heading)     { 'Ajuda' }
-    let(:page_title)  { 'Ajuda' } 
+    let(:heading)     { I18n.t('help') }
+    let(:page_title)  { I18n.t('help') } 
     it_should_behave_like "all static_pages"
   end
 
   describe "Sobre page" do
     before { visit about_path }
 
-    let(:heading)     { 'Sobre Nós' }
-    let(:page_title)  { 'Sobre Nós' } 
+    let(:heading)     { I18n.t('about') }
+    let(:page_title)  { I18n.t('about') } 
     it_should_behave_like "all static_pages"
   end
 
   describe "Entre em contato page" do
     before { visit contact_path }
  
-    let(:heading)     { 'Entre em contato' }
-    let(:page_title)  { 'Entre em contato' } 
+    let(:heading)     { I18n.t('contact')  }
+    let(:page_title)  { I18n.t('contact') } 
     it_should_behave_like "all static_pages"
   end
 
   it "should have the right links on the layout" do
     visit root_path
-    click_link "Sobre"
+    click_link I18n.t('about')
     should have_selector 'title', text: full_title('Sobre Nós')
-    click_link "Ajuda"
+    click_link I18n.t('help')
     should have_selector 'title', text: full_title('Ajuda')
-    click_link "Entre em contato"
+    click_link I18n.t('contact')
     should have_selector 'title', text: full_title('Entre em contato')
-    click_link "Home"
+    click_link I18n.t('home')
     click_link "atlântico português"
     should have_selector 'title', text: full_title('')
     click_link I18n.t('documents.title')
@@ -63,9 +64,9 @@ describe "Static pages" do
 
   it "should have the sign in/out links" do
     visit root_path
-    should have_link('Entrar',      href: signin_path) 
-    should_not have_link('Sair',      href: signout_path) 
-    should_not have_link('Usuários') 
-    should_not have_link('Páginas', href: pages_path) 
+    should have_link(I18n.t('signin_title'), href: signin_path) 
+    should_not have_link(I18n.t('signout_tile'), href: signout_path) 
+    should_not have_link(I18n.t('users_link')) 
+    should_not have_link(I18n.t('pages_link'), href: pages_path) 
   end
 end
