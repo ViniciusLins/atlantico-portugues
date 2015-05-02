325fc3e7dce1deb28f5df017db7422bd42f6daad
Vlins <vlinsufba@gmail.com>
2015-05-02 01:16:10 -0300

    View for 404 running and two others also implemented - final

--- config/application.rb
+++ config/application.rb
@@ -33,29 +33,5 @@ module SampleApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
  
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from Exception, with: :not_found
-  rescue_from ActionController::RoutingError, with: :not_found
-
-  def raise_not_found
-    raise ActionController::RoutingError.new("No route matche #{params[:unmatched_route]}")
-  end
-
-  def not_found
-    respond_to do |format|
-      format.html { render :file => "#{Rails.root}/app/views/errors/file_not_found.html.erb", :layout => false, :status => :not_found }
-      format.xml { head :not_found }
-      format.any { head :not_found }
-    end
-  end
-
-  def error
-    respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/500", :layout => false, :status => :error }
-      format.xml { head :not_found }
-      format.any { head :not_found }
-    end
-  end
-
   end
 end
